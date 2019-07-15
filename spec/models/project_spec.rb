59282e73b48fe71ba062b50ed66e1d8e8c07e1ea
yukoku <powerpythoncc@gmail.com>
2019-07-15 16:18:07 +0900

    add project validation

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :name }
+
+  it "is valid with name, description and due_on" do
+    project = Project.create(
+      name: "Test Project",
+      description: "This is test project",
+      due_on: 1.day.ago
+    )
+    expect(project).to be_valid
+  end
 end
