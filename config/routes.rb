458eac8c81f0f7b28f17027919e50d4656e20938
yukoku <powerpythoncc@gmail.com>
2019-07-18 13:02:38 +0900

    add gems letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   resources :projects
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
