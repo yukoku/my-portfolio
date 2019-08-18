52cdf9a813d259e00e4fcca94c8f5ad42f6819c8
yukoku <powerpythoncc@gmail.com>
2019-08-18 17:51:39 +0900

    remove not using method

--- app/helpers/project_members_helper.rb
+++ app/helpers/project_members_helper.rb
@@ -1,5 +1,2 @@
 module ProjectMembersHelper
-  def accept_project_invitation_url(project, project_member, **parameters)
-    project_member_url(project, project_member) + '?' + parameters.to_query
-  end
 end
