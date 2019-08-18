0153739cdcc257d7cdecf4467f6eba4831e04cf5
yukoku <powerpythoncc@gmail.com>
2019-08-18 17:41:43 +0900

    add dependent destroy to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :projects, through: :project_members
   has_many :assigned_tickets, class_name: "Ticket", foreign_key: "assignee_id"
   has_many :created_tickets, class_name: "Ticket", foreign_key: "creator_id"
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   before_destroy :update_tickets_items_to_nil
 
