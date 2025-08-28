37123e410488c9c2fcbed000fa6a8e61acf1873c
yukoku <powerpythoncc@gmail.com>
2025-08-28 21:04:13 +0900

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Faker::Config.locale = :ja
 admin_user = User.new(
   name: "admin",
   email: "admin@example.com",
-  password: ENV['ADMIN_PASSWORD'],
+  password: 'adminadmin',
   admin: true)
 admin_user.skip_confirmation!
 admin_user.save!
